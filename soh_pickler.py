2532f965db9929751ac04130ef78ade5f3339f97
Kevin Du <kevinkldu@gmail.com>
2017-02-12 23:05:34 -0800

    Update soh_pickler.py

--- soh_pickler.py
+++ soh_pickler.py
@@ -16,11 +16,14 @@ answers.dropna(inplace=True)
 cv = CountVectorizer(stop_words='english', token_pattern="\\b[a-zA-Z][a-zA-Z]+\\b", min_df=10)
 answers_vecs = cv.fit_transform(answers.Body.values)
 
-with open(os.getcwd() + '/input.pkl', 'wb') as picklefile:
+if not os.path.isdir('models'):
+    os.mkdir('models')
+
+with open(os.getcwd() + '/models/input.pkl', 'wb') as picklefile:
     pickle.dump(cv, picklefile)
     
-with open(os.getcwd() + '/answers.pkl', 'wb') as picklefile:
+with open(os.getcwd() + '/models/answers.pkl', 'wb') as picklefile:
     pickle.dump(answers, picklefile)
     
-with open(os.getcwd() + '/answers_vecs.pkl', 'wb') as picklefile:
+with open(os.getcwd() + '/models/answers_vecs.pkl', 'wb') as picklefile:
     pickle.dump(answers_vecs, picklefile)
